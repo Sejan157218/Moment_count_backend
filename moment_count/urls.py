e12b82224a525ebbf5a73c7faa64cc43896850ba
sejan mahmud <sejan157218@gmail.com>
2024-07-03 23:08:48 +0600

    change vercel json----

--- moment_count/urls.py
+++ moment_count/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watch.urls')),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, 
-#                           document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, 
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
